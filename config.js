7fb9cae4567c49f7953d07d60ee4ecae0a6165c4
chnak <chnak@163.com>
2015-10-20 00:37:36 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,4 @@ exports.site_name = 'Node TODO';
 exports.site_desc = 'Very simple todo, demo for connect web dev.';
 exports.session_secret = 'todo session secret';
 
-exports.db = {provider: 'sqLite' , databaseName: 'MyTodoDatabase'}
+exports.db = {provider: 'sqlite' , databaseName: 'MyTodoDatabase'}
